c4e8a0bb8fceffbf41c74cf17b8bb9f7d95220d1
jasonbarry <jasbarry@me.com>
2020-02-03 15:22:28 -0800

    Using emotion for hover states

--- src/Button.js
+++ src/Button.js
@@ -1,33 +1,43 @@
 import React from 'react'
+import { css } from '@emotion/core'
 
 import nextArrow from './img/next.svg'
 import previousArrow from './img/previous.svg'
 
-const containerStyles = {
-  alignItems: 'center',
-  display: 'flex',
-  height: 'calc(100vh - 64px)',
-  justifyContent: 'center',
-  minWidth: '25vw',
-}
+const container = css`
+  align-items: center;
+  display: flex;
+  height: calc(100vh - 64px);
+  justify-content: center;
+  min-width: 25vw;
+`
 
-const buttonStyles = {
-  background: 'none',
-  border: 0,
-  cursor: 'pointer',
-  position: 'fixed',
-}
+const button = css`
+  background: none;
+  border: 0;
+  cursor: pointer;
+  position: fixed;
+`
 
-const imageStyles = {
-  opacity: 0.15,
-}
+const svg = css`
+  opacity: 0.15;
+  transition: opacity 0.2s ease, transform 0.2s ease;
+  &:hover {
+    opacity: 0.25;
+    transform: scale(1.1);
+  }
+  &:active {
+    opacity: 0.25;
+    transform: scale(1);
+  }
+`
 
 export default ({ direction, onClick }) => {
   const arrow = direction === 'previous' ? previousArrow : nextArrow
   return (
-    <div style={containerStyles}>
-      <button onClick={onClick} style={buttonStyles}>
-        <img alt={direction} height={48} src={arrow} style={imageStyles} width={48} />
+    <div css={container}>
+      <button css={button} onClick={onClick}>
+        <img alt={direction} css={svg} height={48} src={arrow} width={48} />
       </button> 
     </div>
   )
--- src/Steps.js
+++ src/Steps.js
@@ -1,41 +1,46 @@
 import React from 'react'
+import { css } from '@emotion/core'
 
-const containerStyles = {
-  background: 'white',
-  borderRadius: 100,
-  bottom: 0,
-  lineHeight: 0,
-  position: 'fixed',
-  padding: 16,
-  paddingBottom: 32,
-  textAlign: 'center',
-  width: '100%',
-}
+const container = css`
+  background: white;
+  border-radius: 100px;
+  bottom: 0;
+  line-height: 0;
+  position: fixed;
+  padding: 16px;
+  padding-bottom: 32px;
+  text-align: center;
+  width: 100%;
+`
 
 export default ({ currentStep, totalSteps }) => {
   const arr = Array(totalSteps)
   const steps = arr.fill(<div />)
   return (
-    <div style={containerStyles}>
+    <div css={container}>
       {steps.map((step, i) => {
         const isActive = currentStep === i + 1
-        const dot = {
-          backgroundColor: isActive ? '#05c5cc' : '#d9d9d9',
-          borderRadius: 8,
-          display: 'inline-block',
-          height: 8,
-          marginLeft: 5,
-          marginRight: 5,
-          transform: `scale(${isActive ? 1.5 : 1})`,
-          transition: 'transform 0.3s ease, background-color 0.3s ease',
-          width: 8,
-        }
+        const dot = css`
+          background-color: ${isActive ? '#05c5cc' : '#d9d9d9'};
+          border-radius: 8px;
+          display: inline-block;
+          height: 8px;
+          margin-left: 5px;
+          margin-right: 5px;
+          transform: scale(${isActive ? 1.5 : 1});
+          transition: transform 0.3s ease, background-color 0.3s ease;
+          width: 8px;
+          &:hover {
+            background-color: ${isActive ? '#05c5cc' : '#bfbfbf'};
+            transform: scale(${isActive ? 1.5 : 1.2});
+          }
+        `
 
         return (
           <a
             key={`step-${i}`}
+            css={dot}
             href={`/${i}`}
-            style={dot}
           />
         )
       })}
