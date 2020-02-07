0f140e46990c9f683759ecf9e113ed8d2af8be4d
jasonbarry <jasbarry@me.com>
2020-02-07 12:17:38 -0800

    Added demo logo

--- src/Logo.js
+++ src/Logo.js
@@ -1,11 +1,12 @@
 import React from 'react'
 
 import logo from './img/logo.svg'
+import logoDemo from './img/logo-demo.svg'
 
-export default ({ width }) => {
+export default ({ demo, width }) => {
   return (
     <a href="https://featurepeek.com" target="_blank">
-      <img alt="FeaturePeek logo" src={logo} width={width || 263} />
+      <img alt="FeaturePeek logo" src={demo ? logoDemo : logo} width={width || 263} />
     </a>
   )
 }
