42f77807b88a4abfca02f2c02b5c49325a168b9b
Jason Barry <jasbarry@me.com>
2020-02-21 13:47:32 -0800

    Adding segment analytics (#16)

---
+++ src/html.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+import { segment } from './utils/analytics'
+
+export default function HTML(props) {
+  return (
+    <html {...props.htmlAttributes}>
+      <head>
+        <meta charSet="utf-8" />
+        <meta httpEquiv="x-ua-compatible" content="ie=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        {props.headComponents}
+      </head>
+      <body {...props.bodyAttributes}>
+        {props.preBodyComponents}
+        <div key={`body`} id="___gatsby" dangerouslySetInnerHTML={{ __html: props.body }} />
+        {props.postBodyComponents}
+        <script key={`segment`} type="text/javascript" dangerouslySetInnerHTML={{ __html: segment }} />
+      </body>
+    </html>
+  )
+}
