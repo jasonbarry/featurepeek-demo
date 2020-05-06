c48c05784f93da5a68aa238236fe9b5c6abcd373
Jason Barry <jason@featurepeek.com>
2020-05-06 14:53:46 -0700

    Refresh logo (#22)
    * Refresh logo
    
    * Add SVG loader

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -2,20 +2,21 @@ import React from 'react'
 import { navigate } from '@reach/router'
 import { css } from '@emotion/core'
 
-import logo from '../img/logo.svg'
-import logoDemo from '../img/logo-demo.svg'
-
-const styles = css`
-  -webkit-appearance: none;
-  background: none;
-  border: 0;
-  cursor: pointer;
-`
+import Logo from '../img/full-logo.svg'
 
 export default ({ demo, width }) => {
+  
+  const styles = css`
+    -webkit-appearance: none;
+    background: none;
+    border: 0;
+    cursor: pointer;
+    width: ${width || 260};
+  `
+
   return (
     <button className="logo" css={styles} onClick={() => navigate(demo ? '/1' : '/0')}>
-      <img alt="FeaturePeek logo" src={demo ? logoDemo : logo} width={width || 263} />
+      <Logo width={width || 263} />
     </button>
   )
 }
